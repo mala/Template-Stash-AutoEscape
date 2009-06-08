bf8ddec08b5aebbfe52e387fb968b051e6042043
mala <github@ma.la>
2009-06-08 22:25:47 +0900

    initial commit

---
+++ lib/Template/Stash/AutoEscape.pm
@@ -0,0 +1,33 @@
+package Template::Stash::AutoEscape;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Template::Stash::AutoEscape -
+
+=head1 SYNOPSIS
+
+  use Template::Stash::AutoEscape;
+
+=head1 DESCRIPTION
+
+Template::Stash::AutoEscape is
+
+=head1 AUTHOR
+
+mala E<lt>cpan@ma.laE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

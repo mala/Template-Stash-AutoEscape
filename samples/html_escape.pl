705cdee97e6bba818b25a9bdb999394dc97af865
mala <github@ma.la>
2009-06-08 22:57:52 +0900

    add sample

---
+++ samples/html_escape.pl
@@ -0,0 +1,80 @@
+#!/usr/local/bin/perl
+
+use strict;
+use Template;
+
+use lib qw(../lib);
+use Template::Stash::AutoEscape;
+use Template::Stash::AutoEscape::Escaped::HTML;
+
+my $tmpl = join '', <DATA>;
+
+my $html_class = Template::Stash::AutoEscape->class_for('HTML');
+
+sub my_html_filter {
+    my $text = shift;
+    if ( ref $text eq $html_class ) {
+        warn "already escaped: " . $text;
+        return $text;
+    }
+    for ($text) {
+        s/&/&amp;/g;
+        s/</&lt;/g;
+        s/>/&gt;/g;
+        s/"/&quot;/g;
+    }
+    return $text;
+}
+
+my $stash = Template::Stash::AutoEscape->new(
+    escape_type    => "HTML",    # default => HTML
+    method_for_raw => "raw",     # default => raw
+);
+use URI;
+my $tt = Template->new(
+    {
+        STASH   => $stash,
+        FILTERS => { html => \&my_html_filter, },
+    }
+);
+
+my $data = {
+    uri         => URI->new("http://example.com/?a=1&b=2"),
+    array       => [qw(<b> <a> </a>)],
+    string      => "<b>hoge</b>",
+    html_string => as_html("<b>hoge</b>"),
+    object      => {
+        hoge   => "<b>hoge</b>",
+        method => sub {
+            "<b>method</b>";
+          }
+    },
+};
+
+sub as_html {
+    Template::Stash::AutoEscape->class_for('HTML')->new( $_[0] );
+}
+
+$tt->process( \$tmpl, $data, \my $output ) or die $tt->error;
+print $output;
+
+__DATA__
+<html>
+
+[% uri %]
+[% uri | html  %]
+
+[% html_string %] => <b>hoge</b>
+[% html_string | html %] => <b>hoge</b>
+
+[% string %] => &lt;b&gt;hoge&lt;/b&gt;
+[% string | html %] => &lt;b&gt;hoge&lt;/b&gt;
+[% string.raw %] => <b>hoge</b>
+[% string.raw | html %] => &lt;b&gt;hoge&lt;/b&gt;
+
+[% object.hoge %] => &lt;b&gt;hoge&lt;/b&gt;
+[% object.method %] => &lt;b&gt;method&lt;/b&gt;
+[% object.hoge.raw %] => <b>hoge</b>
+[% object.method.raw %] => <b>method</b>
+
+</html>

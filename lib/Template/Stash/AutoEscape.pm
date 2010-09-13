373f0ac7e4179588e572209fea3f4ebc8d5b10d3
mala <github@ma.la>
2010-09-13 19:28:13 +0900

    Checking in changes prior to tagging of version 0.03.  Changelog diff is:

--- lib/Template/Stash/AutoEscape.pm
+++ lib/Template/Stash/AutoEscape.pm
@@ -2,7 +2,7 @@ package Template::Stash::AutoEscape;
 
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Template::Config;
 use base ($Template::Config::STASH, 'Class::Data::Inheritable');
